c2dd818871d55d39686d97e8ac57cbad151b35ed
Lucas Blanc <78850304+Lucxs95@users.noreply.github.com>
2024-02-20 10:12:36 +0100

    add

--- lib/user_profile_screen.dart
+++ lib/user_profile_screen.dart
@@ -1,9 +1,10 @@
+import 'dart:convert';
 import 'dart:html';
-import 'package:intl/intl.dart';
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:http/http.dart' as http;
-import 'dart:convert';
+import 'package:intl/intl.dart';
+import 'package:image_picker/image_picker.dart';
 
 Future<String?> getJwtToken() async {
   final prefs = await SharedPreferences.getInstance();
@@ -27,23 +28,24 @@ class _UserProfileScreenState extends State<UserProfileScreen> {
   final TextEditingController _cityController = TextEditingController();
   final TextEditingController _loginController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
-  
+  String _profileImageUrl = 'https://tr.rbxcdn.com/70108dc7da4e002c8e5d2c1dcf0825fb/420/420/Hat/Png';
+
   @override
   void initState() {
     super.initState();
     fetchUserProfile();
   }
 
-Future<void> fetchUserProfile() async {
-  final jwtToken = await getJwtToken();
-  final userId = await getUserId(); // Assurez-vous d'obtenir l'userId
-  final response = await http.get(
-    Uri.parse('http://localhost:5000/api/user/profile/$userId'), // Utilisez l'userId dans l'URL
-    headers: {
-      'Content-Type': 'application/json; charset=UTF-8',
-      'Authorization': 'Bearer $jwtToken',
-    },
-  );
+  Future<void> fetchUserProfile() async {
+    final jwtToken = await getJwtToken();
+    final userId = await getUserId();
+    final response = await http.get(
+      Uri.parse('http://localhost:5000/api/user/profile/$userId'),
+      headers: {
+        'Content-Type': 'application/json; charset=UTF-8',
+        'Authorization': 'Bearer $jwtToken',
+      },
+    );
 
     if (response.statusCode == 200) {
       final data = jsonDecode(response.body);
@@ -58,35 +60,59 @@ Future<void> fetchUserProfile() async {
         _passwordController.text = data['password'] ?? '';
 
       });
-    } else {
-      // Handle the error
-      print('Failed to fetch user profile');
     }
   }
 
-  Future<void> saveUserProfile() async {
-    final jwtToken = await getJwtToken();
-    final userId = await getUserId();
-    final response = await http.patch(
-      Uri.parse('http://localhost:5000/api/user/updateProfile/$userId'),
-      headers: {
-        'Content-Type': 'application/json',
-        'Authorization': 'Bearer $jwtToken',
-      },
-      body: jsonEncode({
-        'login': _loginController.text,
-        'password': _passwordController.text,
-        'birthday': _birthdayController.text,
-        'address': _addressController.text,
-        'postalCode': _postalCodeController.text,
-        'city': _cityController.text,
-      }),
-    );
+Future<void> saveUserProfile() async {
+  final jwtToken = await getJwtToken();
+  final userId = await getUserId();
 
-    if (response.statusCode == 200) {
-      ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text("Profile updated successfully")));
-    } else {
-      ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text("Failed to update profile")));
+  // Convertir la date du format dd/MM/yyyy en un objet DateTime
+  DateTime? birthday;
+  try {
+    final parts = _birthdayController.text.split('/');
+    if (parts.length == 3) {
+      final day = int.parse(parts[0]);
+      final month = int.parse(parts[1]);
+      final year = int.parse(parts[2]);
+      birthday = DateTime(year, month, day);
+    }
+  } catch (e) {
+    print("Error parsing birthday: $e");
+    ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text("Invalid birthday format")));
+    return;
+  }
+
+  final response = await http.patch(
+    Uri.parse('http://localhost:5000/api/user/updateProfile/$userId'),
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': 'Bearer $jwtToken',
+    },
+    body: jsonEncode({
+      'email': _loginController.text,
+      // Ne pas envoyer le mot de passe s'il ne doit pas être modifié ou géré différemment
+      'birthday': birthday != null ? DateFormat('yyyy-MM-dd').format(birthday) : null, // Convertit en format ISO 8601
+      'address': _addressController.text,
+      'postalCode': _postalCodeController.text,
+      'city': _cityController.text,
+    }),
+  );
+
+  if (response.statusCode == 200) {
+    ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text("Profile updated successfully")));
+  } else {
+    ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text("Failed to update profile")));
+  }
+}
+
+  Future<void> pickImage() async {
+    final ImagePicker _picker = ImagePicker();
+    final XFile? image = await _picker.pickImage(source: ImageSource.gallery);
+    if (image != null) {
+      setState(() {
+        _profileImageUrl = image.path;
+      });
     }
   }
 
@@ -98,26 +124,87 @@ Future<void> fetchUserProfile() async {
         padding: EdgeInsets.all(16.0),
         child: Column(
           children: <Widget>[
-            TextField(controller: _loginController, decoration: InputDecoration(labelText: 'Login'), readOnly: true),
-            TextField(controller: _passwordController, decoration: InputDecoration(labelText: 'Password'), obscureText: true, readOnly: true),
-            TextField(controller: _birthdayController, decoration: InputDecoration(labelText: 'Birthday')),
-            TextField(controller: _addressController, decoration: InputDecoration(labelText: 'Address')),
-            TextField(controller: _postalCodeController, decoration: InputDecoration(labelText: 'Postal Code'), keyboardType: TextInputType.number),
-            TextField(controller: _cityController, decoration: InputDecoration(labelText: 'City')),
+            GestureDetector(
+              onTap: pickImage,
+              child: CircleAvatar(
+                radius: 50,
+                backgroundImage: NetworkImage(_profileImageUrl),
+              ),
+            ),
             SizedBox(height: 20),
-            ElevatedButton(onPressed: saveUserProfile, child: Text('Validate')),
-            ElevatedButton(onPressed: logout, child: Text('Logout'), style: ElevatedButton.styleFrom(primary: Colors.red)),
+            TextField(
+              controller: _loginController,
+              decoration: InputDecoration(
+                labelText: 'Email',
+                border: OutlineInputBorder(),
+                prefixIcon: Icon(Icons.person),
+              ),
+                enabled: false, // Désactive le champ de texte
+
+            ),            
+            SizedBox(height: 20),
+            TextField(
+              controller: _passwordController,
+              decoration: InputDecoration(
+                labelText: 'Mot de passe',
+                border: OutlineInputBorder(),
+                prefixIcon: Icon(Icons.lock),
+
+              ),
+                obscureText: true, // Ceci obfusque le texte entré dans le champ de texte
+                enabled: false, // Désactive le champ de texte
+
+            ),
+            SizedBox(height: 20),
+            TextField(
+              controller: _birthdayController,
+              decoration: InputDecoration(
+                labelText: 'Birthday',
+                border: OutlineInputBorder(),
+                prefixIcon: Icon(Icons.cake),
+              ),
+            ),
+            SizedBox(height: 20), // Ajoute un espace de 20 pixels en dessous du champ de texte
+
+            TextField(
+              controller: _addressController,
+              decoration: InputDecoration(
+                labelText: 'Address',
+                border: OutlineInputBorder(),
+                prefixIcon: Icon(Icons.home),
+              ),
+            ),
+            SizedBox(height: 20), // Ajoute un espace de 20 pixels en dessous du champ de texte
+TextField(
+              controller: _postalCodeController,
+              decoration: InputDecoration(
+                labelText: 'Postal Code',
+                border: OutlineInputBorder(),
+                prefixIcon: Icon(Icons.local_post_office),
+              ),
+              keyboardType: TextInputType.number,
+            ),
+            SizedBox(height: 20), // Ajoute un espace de 20 pixels en dessous du champ de texte
+TextField(
+              controller: _cityController,
+              decoration: InputDecoration(
+                labelText: 'City',
+                border: OutlineInputBorder(),
+                prefixIcon: Icon(Icons.location_city),
+              ),
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(onPressed: saveUserProfile, child: Text('Sauvegarder')),
+                        SizedBox(height: 20), // Ajoute un espace de 20 pixels en dessous du champ de texte
+
+            ElevatedButton(onPressed: () async {
+              final prefs = await SharedPreferences.getInstance();
+              await prefs.clear();
+              window.location.reload();
+            }, child: Text('Logout'), style: ElevatedButton.styleFrom(primary: Colors.red)),
           ],
         ),
       ),
     );
-  }
-
-  void logout() async {
-    final prefs = await SharedPreferences.getInstance();
-    await prefs.clear(); 
-   window.location.reload();
-
-
   }
 }
